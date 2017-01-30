7966612a814d34112a9315f05d4177b006d1152f
Shashank K <keshavashashank@gmail.com>
2017-01-30 23:49:31 +0530

    Stores

--- actionType/index.js
+++ actionType/index.js
@@ -1,6 +1,6 @@
 export const ADD_TODO = 'ADD_TODO';
 export const TOGGLE_TODO = 'TOGGLE_TODO';
-export const SET_VISIBILITY_FILTER = 'SET_VISIBILITY_FILTER;
+export const SET_VISIBILITY_FILTER = 'SET_VISIBILITY_FILTER';
 
 export const VisibityFilter = {
     SHOW_ALL: 'SHOW_ALL',
